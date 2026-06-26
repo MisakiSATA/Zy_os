5816e63c77d71ec16d34fd1256d8a98f36281366
MisakiSATA <lllqyc@163.com>
2026-06-26 16:57:51 +0800

    feat: add minimal custom test framework integration

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,9 @@
 
 #![no_std] // Do not link against the Rust standard library
 #![no_main] // Disable all Rust-level entry points
+#![feature(custom_test_frameworks)]
+#![test_runner(crate::test_runner)]
+#![reexport_test_harness_main = "test_main"]
 
 use core::panic::PanicInfo;
 
@@ -18,8 +21,25 @@ fn panic(info: &PanicInfo) -> ! {
 #[unsafe(no_mangle)]
 pub extern "C" fn _start() {
     println!("Hello World{}", "!");
-    panic!("Some panic message");
+    
+    #[cfg(test)]
+    test_main();
     
     loop {}
 }
 
+
+#[cfg(test)]
+pub fn test_runner(tests: &[&dyn Fn()]) {
+    println!("Running {} tests", tests.len());
+    for test in tests {
+        test();
+    }
+}
+
+#[test_case]
+fn trivial_assertion() {
+    println!("trivial assertion... ");
+    assert_eq!(1, 1);
+    println!("[ok]");
+}
