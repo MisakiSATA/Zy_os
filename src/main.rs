3d92bce26471a593bb1c752195ba2cd757570b1f
MisakiSATA <lllqyc@163.com>
2026-06-27 14:54:33 +0800

    test: add qemu isa-debug-exit device for automated test shutdown and configure bootimage qemu exit device and sucess exit code

--- src/main.rs
+++ src/main.rs
@@ -35,6 +35,8 @@ pub fn test_runner(tests: &[&dyn Fn()]) {
     for test in tests {
         test();
     }
+    
+    exit_qemu(QemuExitCode::Sucess);
 }
 
 #[test_case]
@@ -42,4 +44,21 @@ fn trivial_assertion() {
     println!("trivial assertion... ");
     assert_eq!(1, 1);
     println!("[ok]");
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[repr(u32)]
+
+pub enum QemuExitCode {
+    Sucess = 0x10,
+    Failed = 0x11,
+}
+
+pub fn exit_qemu(exit_code: QemuExitCode) {
+    use x86_64::instructions::port::Port;
+    
+    unsafe {
+        let mut port = Port::new(0xf4);
+        port.write(exit_code as u32);
+    }
 }
