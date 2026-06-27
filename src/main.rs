81068eea3c8557b303c6f31cfee86699cf233658
MisakiSATA <lllqyc@163.com>
2026-06-27 16:59:22 +0800

    test: add timeout and automate test status output

--- src/main.rs
+++ src/main.rs
@@ -42,10 +42,10 @@ pub extern "C" fn _start() {
 
 
 #[cfg(test)]
-pub fn test_runner(tests: &[&dyn Fn()]) {
+pub fn test_runner(tests: &[&dyn Testable]) {
     serial_println!("Running {} tests", tests.len());
     for test in tests {
-        test();
+        test.run();
     }
     
     exit_qemu(QemuExitCode::Sucess);
@@ -53,9 +53,7 @@ pub fn test_runner(tests: &[&dyn Fn()]) {
 
 #[test_case]
 fn trivial_assertion() {
-    serial_println!("trivial assertion... ");
     assert_eq!(1, 1);
-    serial_println!("[ok]");
 }
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
@@ -75,3 +73,17 @@ pub fn exit_qemu(exit_code: QemuExitCode) {
     }
 }
 
+pub trait Testable {
+    fn run(&self) -> ();
+}
+
+impl<T> Testable for T
+where
+    T: Fn(),
+{
+    fn run (&self) {
+        serial_println!("{}...\t", core::any::type_name::<T>());
+        self();
+        serial_println!("[ok]");
+    }
+}
