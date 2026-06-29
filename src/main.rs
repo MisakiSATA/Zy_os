ccf53f5fb9c1b79641b3c61dc6ad22092f8a8642
MisakiSATA <lllqyc@163.com>
2026-06-29 16:08:10 +0800

    fix: repair lib-based test infrastructure and serial macros

--- src/main.rs
+++ src/main.rs
@@ -3,15 +3,20 @@
 #![no_std] // Do not link against the Rust standard library
 #![no_main] // Disable all Rust-level entry points
 #![feature(custom_test_frameworks)]
-#![test_runner(crate::test_runner)]
+#![test_runner(ZY_os::test_runner)]
 #![reexport_test_harness_main = "test_main"]
 
 use core::panic::PanicInfo;
+use ZY_os::println;
 
-mod serial;
+// #[macro_use]
+// mod vga_buffer;
 
-#[macro_use]
-mod vga_buffer;
+#[cfg(test)]
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    ZY_os::test_panic_handler(info)
+}
 
 #[cfg(not(test))]
 #[panic_handler]
@@ -21,69 +26,13 @@ fn panic(info: &PanicInfo) -> ! {
     loop {}
 }
 
-#[cfg(test)]
-#[panic_handler]
-fn panic(info: &PanicInfo) -> ! {
-    serial_println!("[failed]\n");
-    serial_println!("Error: {}\n", info);
-    exit_qemu(QemuExitCode::Failed);
-    loop {}
-}
 
 #[unsafe(no_mangle)]
-pub extern "C" fn _start() {
+pub extern "C" fn _start() -> ! {
     println!("Hello World{}", "!");
     
     #[cfg(test)]
     test_main();
     
     loop {}
-}
-
-
-#[cfg(test)]
-pub fn test_runner(tests: &[&dyn Testable]) {
-    serial_println!("Running {} tests", tests.len());
-    for test in tests {
-        test.run();
-    }
-    
-    exit_qemu(QemuExitCode::Sucess);
-}
-
-#[test_case]
-fn trivial_assertion() {
-    assert_eq!(1, 1);
-}
-
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
-#[repr(u32)]
-
-pub enum QemuExitCode {
-    Sucess = 0x10,
-    Failed = 0x11,
-}
-
-pub fn exit_qemu(exit_code: QemuExitCode) {
-    use x86_64::instructions::port::Port;
-    
-    unsafe {
-        let mut port = Port::new(0xf4);
-        port.write(exit_code as u32);
-    }
-}
-
-pub trait Testable {
-    fn run(&self) -> ();
-}
-
-impl<T> Testable for T
-where
-    T: Fn(),
-{
-    fn run (&self) {
-        serial_println!("{}...\t", core::any::type_name::<T>());
-        self();
-        serial_println!("[ok]");
-    }
 }
