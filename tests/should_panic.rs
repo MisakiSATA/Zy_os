81cdb138e53f420006395e36ab5b761e080b1c22
MisakiSATA <lllqyc@163.com>
2026-07-01 14:25:23 +0800

    test: simplify should-panic integration test by disabling harness

--- tests/should_panic.rs
+++ tests/should_panic.rs
@@ -2,9 +2,6 @@
 
 #![no_std]
 #![no_main]
-#![feature(custom_test_frameworks)]
-#![test_runner(test_runner)]
-#![reexport_test_harness_main = "test_main"]
 
 use core::panic::PanicInfo;
 use ZY_os::{QemuExitCode, exit_qemu, serial_println};
@@ -18,25 +15,13 @@ fn panic(_info: &PanicInfo) -> ! {
 
 #[unsafe(no_mangle)]
 pub extern "C" fn _start() -> ! {
-    test_main();
-    
+    should_fail();
+    serial_println!("[test did not panic]");
+    exit_qemu(QemuExitCode::Failed);    
     loop {}
 }
 
-pub fn test_runner(tests: &[&dyn Fn()]) {
-    serial_println!("Running {} tests", tests.len());
-    for test in tests {
-        test();
-        serial_println!("[test did not panic]");
-        exit_qemu(QemuExitCode::Failed);
-    }
-    exit_qemu(QemuExitCode::Success);
-}
-
-use ZY_os::serial_print;
-
-#[test_case]
 fn should_fail() {
-    serial_print!("should_fail...");
+    serial_println!("should_fail... ");
     assert_eq!(0, 1);
 }
